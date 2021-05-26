a89a2a452b4034e92321baab9270075f032b810c
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2021-05-26 14:21:45 +0530

    Improved the redirect page

--- src/pages/redirect.js
+++ src/pages/redirect.js
@@ -1,7 +1,10 @@
+import { Grid, Spinner } from '@chakra-ui/react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../config/firebase';
+import useListenToSocket from '../hooks/useListenToSocket';
 
 const Redirect = () => {
   const router = useRouter();
@@ -18,7 +21,31 @@ const Redirect = () => {
       router.push('/');
     }
   }, [path, router, user]);
-  return <div>Loading ...</div>;
+  useListenToSocket(true, null);
+  return (
+    <>
+      <Head>
+        <title>Conversations | Redirecting ...</title>
+      </Head>
+      <Grid
+        height={'100vh'}
+        width={'100%'}
+        alignSelf={'center'}
+        justifyContent='center'
+      >
+        <Spinner
+          thickness='4px'
+          speed='0.65s'
+          emptyColor='gray.200'
+          color='blue.500'
+          size='xl'
+          alignSelf={'center'}
+          justifyContent='center'
+          label='Redirecting ...'
+        />
+      </Grid>
+    </>
+  );
 };
 
 export default Redirect;
